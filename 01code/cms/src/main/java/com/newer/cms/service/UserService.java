5b2dcd8f9f89ba930663d216245ef80e600b7008
myNameZhuLu <498319853@qq.com>
2019-03-16 22:48:31 +0800

    html页面与登录用户列表实现

--- 01code/cms/src/main/java/com/newer/cms/service/UserService.java
+++ 01code/cms/src/main/java/com/newer/cms/service/UserService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.newer.cms.mapper.UserMapper;
+import com.newer.cms.model.Page;
+import com.newer.cms.pojo.User;
 import com.newer.cms.pojo.UserRole;
 
 @Service
@@ -16,6 +18,78 @@ public class UserService {
 	public List<UserRole> queryUserRole() {
 		List<UserRole> queryUserRole = userMapper.queryUserRole();
 		return queryUserRole;
+	}
+
+	/**
+	 * 获取用户信息
+	 * 
+	 * @param pageNoStr
+	 * @param pageSize
+	 * @return 带分页效果的用户信息 Page对象
+	 */
+	public Page<UserRole> getPageByUser(String pageNoStr, Integer pageSize) {
+		// 得到用户信息总记录数
+		Integer totalUser = userMapper.getTotalUser();
+
+		// 实例化Page对象
+		Page<UserRole> page = new Page<>(pageNoStr, totalUser, pageSize);
+
+		// 修正分页初始记录数
+		Integer index = (page.getPageNo() - 1) * pageSize;
+		System.out.println(totalUser + "......" + index + "...." + pageSize + "....." + page.getPageNo());
+		List<UserRole> data = userMapper.getPageByUser(index, pageSize);
+		page.setData(data);
+		return page;
+	}
+
+	/**
+	 * 用户登录。查询用户信息
+	 * 
+	 * @param userName
+	 * @param password
+	 * @return 成功返回用户信息UserRole对象, 否则返回null，
+	 */
+	public UserRole getUserRole(String userName, String password) {
+		// 通过用户名和密码查询用户并返回
+		return userMapper.getUserRoleByUserNameAndPassword(userName, password);
+	}
+
+	/**
+	 * 新增用户
+	 * 
+	 * @param user
+	 * @return String 成功返回"ok",否则返回"error"
+	 */
+	public String saveUser(User user) {
+		// 新增用户 得到影响行数
+		int i = userMapper.saveUser(user);
+
+		return i > 0 ? "ok" : "error";
+	}
+
+	/**
+	 * 更新用户信息
+	 * 
+	 * @param user
+	 * @return String 成功返回"ok",否则返回"error"
+	 */
+	public String updateUser(User user) {
+		// 更新用户，得到影响行数
+		int i = userMapper.updateUser(user);
+		return i > 0 ? "ok" : "error";
+	}
+
+	/**
+	 * 根据id删除用户
+	 * 
+	 * @param id
+	 * @return String 成功返回“ok",否则返回"error“
+	 */
+	public String deleteUser(Integer id) {
+		// 根据id删除用户数据 得到返回的影响函数
+		int i = userMapper.deleteUser(id);
+
+		return i > 0 ? "ok" : "error";
 	}
 
 }
---
+++ 01code/cms/src/main/java/com/newer/cms/utils/JwtTokenUtil.java
@@ -0,0 +1,73 @@
+package com.newer.cms.utils;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Clock;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.DefaultClock;
+
+/**
+ * 
+ * 牛耳教育,180801J: 创建JWT工具类 <br>
+ * 
+ * @author 朱璐 2019年3月4日
+ */
+@Component
+public class JwtTokenUtil {
+
+	private Clock clock = DefaultClock.INSTANCE;
+
+	/**
+	 * 秘钥，从application.properties中读取
+	 */
+	@Value("${jwt.secret}")
+	private static final String SECRET = "zhulu";
+	/**
+	 * jwt失效时间（单位为秒）
+	 */
+	@Value("${jwt.expiration}")
+	private long expiration = 1800L;
+
+	/**
+	 * 创建jwt token
+	 * 
+	 * @param uid
+	 * @param lastTime
+	 * @param pwd
+	 * @param aid
+	 * @return
+	 */
+	public String creatJwt(String uname, Integer uid, Integer rid) {
+		Date now = clock.now();
+		// 添加JWT的包含部分：有三个部分：头部（header）。截荷（payload),签证（sinnatrue）,
+		// 设置jwt创建时间
+		JwtBuilder jwtBuilder = Jwts.builder().setHeaderParam("typ", "JWT").setIssuedAt(now).setIssuer(uname) // 设置当前用户信息
+				.setExpiration(calculateExpirationDate(now)) // 设置失效时间
+				.setSubject("uid").claim("uname", uname).claim("uid", uid).claim("role", rid)
+				.signWith(SignatureAlgorithm.HS512, SECRET); // 设置加密算法，指定秘钥
+
+		return jwtBuilder.compact();
+	}
+
+	/**
+	 * 验证JWT
+	 * 
+	 * @param token
+	 * @return
+	 */
+	public Claims parseJWT(String token) {
+		// 根据秘钥解析token，解析成功返回claims;解析不成功会抛出异常
+		Claims claims = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody();
+		return claims;
+	}
+
+	private Date calculateExpirationDate(Date createdDate) {
+		return new Date(createdDate.getTime() + expiration * 1000);
+	}
+}
