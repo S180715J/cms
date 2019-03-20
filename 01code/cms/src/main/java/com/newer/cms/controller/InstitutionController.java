18caf780c1e507c7b2195d1b9dee1bec516482c5
myNameZhuLu <498319853@qq.com>
2019-03-20 19:43:58 +0800

    1.0.7版

--- 01code/cms/src/main/java/com/newer/cms/controller/InstitutionController.java
+++ 01code/cms/src/main/java/com/newer/cms/controller/InstitutionController.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.newer.cms.model.Page;
 import com.newer.cms.pojo.Institution;
 import com.newer.cms.service.InstitutionService;
 
@@ -23,6 +27,13 @@ public class InstitutionController {
 	@Autowired
 	private InstitutionService inService;
 
+	/**
+	 * 用户添加所用的机构下拉框
+	 * 
+	 * @param code
+	 * @param type
+	 * @return
+	 */
 	@GetMapping("/findInstitution")
 	public ResponseEntity<?> findInstitution(String code, Integer type) {
 
@@ -42,7 +53,7 @@ public class InstitutionController {
 	 */
 	@GetMapping("/findTree")
 	public ResponseEntity<?> getInstitutionTree(Integer id) {
-		List<Institution> list = inService.findInstitutionTree(id);
+		List<Institution> list = inService.findInstitutionTree();
 		if (list == null) {
 			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 		}
@@ -50,4 +61,63 @@ public class InstitutionController {
 		return new ResponseEntity<List<Institution>>(list, HttpStatus.OK);
 
 	}
+
+	/**
+	 * 得到当前页的机构信息
+	 * 
+	 * @param pageNoStr
+	 * @param pageSizeStr
+	 * @return
+	 */
+	@GetMapping("/findPageInstitution")
+	public ResponseEntity<?> findPageInstitutions(
+			@RequestParam(value = "pageNoStr", required = false, defaultValue = "1") String pageNoStr,
+			@RequestParam(value = "pageSizeStr", required = false, defaultValue = "10") String pageSizeStr) {
+
+		// 将每页显示记录数转换
+		Integer pageSize = Integer.parseInt(pageSizeStr);
+
+		Page<Institution> page = inService.findPageInstitions(pageNoStr, pageSize);
+
+		if (page == null) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+		page.setStatus(HttpStatus.OK);
+		return new ResponseEntity<Page<Institution>>(page, HttpStatus.OK);
+	}
+
+	/**
+	 * 新增机构
+	 * 
+	 * @param institution
+	 * @return
+	 */
+	@PostMapping("/institution")
+	public ResponseEntity<?> saveInstitution(@RequestBody Institution institution) {
+
+		// 调用服务层的方法
+		String data = inService.saveInstitution(institution);
+		if (data == null) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<String>(data, HttpStatus.OK);
+	}
+
+	/**
+	 * 新增子机构/部门
+	 * 
+	 * @param institution
+	 * @return 成功返回ok
+	 */
+	@PostMapping("/mechanism")
+	public ResponseEntity<?> saveMechanism(@RequestBody Institution institution) {
+		System.out.println(institution);
+		// 调用服务层
+		String data = inService.saveMechanism(institution);
+		if (data == null) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		return new ResponseEntity<String>(data, HttpStatus.OK);
+	}
+
 }
