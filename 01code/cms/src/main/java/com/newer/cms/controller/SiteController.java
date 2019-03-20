fbe218127747903f7ed5579e18f446abc73a3efc
gongjiangpeng <48433749+gongjiangpeng123@users.noreply.github.com>
2019-03-20 09:27:17 +0800

    1.0.5

--- 01code/cms/src/main/java/com/newer/cms/controller/SiteController.java
+++ 01code/cms/src/main/java/com/newer/cms/controller/SiteController.java
@@ -6,11 +6,17 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.newer.cms.model.Page;
+import com.newer.cms.pojo.Site;
 import com.newer.cms.pojo.SiteUser;
 import com.newer.cms.service.SiteService;
 
@@ -24,16 +30,14 @@ public class SiteController {
 	@GetMapping("/getPageSiteUser")
 	public ResponseEntity<?> getPageSiteUser(
 			@RequestParam(value = "pageNoStr", required = false, defaultValue = "1") String pageNoStr,
-			@RequestParam(value = "pageSizeStr", required = false, defaultValue = "10") Integer PageSizeStr
+			@RequestParam(value = "pageSizeStr", required = false, defaultValue = "10") Integer PageSizeStr,
+			String name
 		){
 		System.out.println("PageSizeStr:"+PageSizeStr);
 		Map<String,Object> param=new HashMap<>();
 		param.put("pageNoStr", pageNoStr);
 		param.put("pageSize", PageSizeStr);
-		
-			param.put("name", null);
-	
-		
+		param.put("name", name);
 		Page<SiteUser> page = siteService.getPageSiteUser(param);
 		page.setStatus(HttpStatus.OK);
 		System.out.println(page.toString());
@@ -42,5 +46,30 @@ public class SiteController {
 		}
 		return new ResponseEntity<Page>(page, HttpStatus.OK);
 	}
+	
+	//删除站点
+	@DeleteMapping("/site/{id}")
+	public ResponseEntity<?> deleteSite(@PathVariable("id") Integer id){
+				if (id == null) {
+					return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+				}
+				int i=siteService.deleteSite(id);
+				return new ResponseEntity<>(i>0?"1":"0",HttpStatus.OK);
+	}
+	
+	//添加站点
+	@PostMapping("/sites")
+	public ResponseEntity<?> addSite(@RequestBody Site site){
+		System.out.println(site);
+		int i=siteService.addSite(site);
+		return new ResponseEntity<>(i>0?"1":"0",HttpStatus.OK);
+	}
 
+	//修改站点名
+	@PostMapping("/putSite")
+	public ResponseEntity<?> putSite(@RequestBody Site site){
+		System.out.println(site);
+		int i =siteService.putSite(site);
+		return new ResponseEntity<>(i>0?"1":"0",HttpStatus.OK);
+	}
 }
--- 01code/cms/src/main/java/com/newer/cms/service/SiteService.java
+++ 01code/cms/src/main/java/com/newer/cms/service/SiteService.java
@@ -5,9 +5,13 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.newer.cms.mapper.LanguageMapper;
 import com.newer.cms.mapper.SiteMapper;
 import com.newer.cms.model.Page;
+import com.newer.cms.pojo.Language;
+import com.newer.cms.pojo.Site;
 import com.newer.cms.pojo.SiteUser;
 
 @Service
@@ -15,11 +19,15 @@ public class SiteService {
 	
 	@Autowired
 	private SiteMapper siteMapper;
+	
+	@Autowired
+	private LanguageMapper languageMapper;
 	/**
 	 *  .获得分页对象
 	 * @param param
 	 * @return
 	 */
+	@Transactional
 	public Page<SiteUser> getPageSiteUser(Map<String,Object> param){
 		//得到站点的总计录数
 		int totalSite=siteMapper.getTotalSite(param);
@@ -38,6 +46,29 @@ public class SiteService {
 		
 		return page;
 		
+	}
+	
+	@Transactional
+	public int deleteSite(Integer id) {
+		int i = siteMapper.deleteSite(id);
+		if(i>0) {
+			List<Language> q = languageMapper.queryLanguage(id);
+			System.out.println(q);
+			if(q!=null) {
+				languageMapper.deleteLanguages(id);
+			}
+		}
+		return i;
+	}
+
+	public int addSite(Site site) {
+		int i = siteMapper.saveSite(site);
+		return i;
+	}
+
+	public int putSite(Site site) {
+		int updateSite = siteMapper.updateSite(site);
+		return updateSite;
 	}
 
 }
